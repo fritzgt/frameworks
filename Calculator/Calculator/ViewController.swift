f8b5db01b2159e19009b20c9839b741fd909532c
Fritz <fritz@icloud.com>
2022-04-10 16:14:42 -0500

    add abaction and outlets

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,14 +8,39 @@
 import UIKit
 import RPN
 
-class ViewController: UIViewController {
-//    let calculator = Calculator
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+//MARK: - Outlets
+    @IBOutlet weak var textLabel: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        textLabel.delegate = self
     }
 
-
+//MARK: - Actions
+    @IBAction func numberButtonTap(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
+    @IBAction func decimalButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func minusButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiplyButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func deviceButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func returnButtonTapped(_ sender: Any) {
+    }
+    
 }
 
